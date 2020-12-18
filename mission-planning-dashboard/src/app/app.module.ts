9e6a0ec0e16701cbb25e71457024d2952f2a717a
Christopher <c.millerscott@gmail.com>
2020-12-17 20:22:39 -0600

    second commit

--- mission-planning-dashboard/src/app/app.module.ts
+++ mission-planning-dashboard/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CrewComponent } from './crew/crew.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { ExpiramentsComponent } from './expiraments/expiraments.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CrewComponent,
+    EquipmentComponent,
+    ExpiramentsComponent,
+    ExperimentsComponent
   ],
   imports: [
     BrowserModule
